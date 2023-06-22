ecf85a79498b6ec289d3938750837b34c1d584fd
Mehmet Kaptan <102242503+KaptanW@users.noreply.github.com>
2023-06-23 01:07:37 +0300

    Proje dosyası ekle.

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace GelirGiderProgramıASPNETCORE.Models
+   
+{
+    public class ApplicationDbContext :DbContext
+    {
+
+
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+        }
+
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
